a3f5a8bfedcdba02302163ffcd015a2b35936a92
srivatsavr02 <srivatsavr02@gmail.com>
2022-01-29 11:22:10 +0530

    done

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.API_KEY);
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "inr",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+app.listen(process.env.PORT || 3000);
+
+// Example endpoint
+// http://localhost:5001/react-c4e51/us-central1/api
